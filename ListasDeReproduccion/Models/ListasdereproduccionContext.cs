308f23becef92f81954d9576681184dffcc1ecb7
AngelPineda1 <201G0276@rcarbonifera.tecnm.mx>
2023-03-22 08:49:55 -0600

    Cambios vista canciones por lista

--- ListasDeReproduccion/Models/ListasdereproduccionContext.cs
+++ ListasDeReproduccion/Models/ListasdereproduccionContext.cs
@@ -21,7 +21,7 @@ public partial class ListasdereproduccionContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;user=root;database=listasdereproduccion;password=9hn2425s2O", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.28-mysql"));
+        => optionsBuilder.UseMySql("server=localhost;user=root;database=listasdereproduccion;password=root", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.28-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
